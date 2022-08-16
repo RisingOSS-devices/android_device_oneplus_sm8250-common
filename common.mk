e3d02e2c56e20e27d5ac8f3f6da2ef02354e2179
cjh1249131356 <cjh1249131356@gmail.com>
2022-08-16 18:30:30 +0800

    sm8250-common: Add support for OnePlus Camera & Gallery
    * Camera repo: https://bitbucket.org/nameless-aosp/vendor_oneplus_camera
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: TheMysticle <36317711+TheMysticle@users.noreply.github.com>
    Co-authored-by: TheMysticle <36317711+TheMysticle@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -103,7 +103,11 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    Aperture \
+    libcamera2ndk_vendor \
+    libstdc++_vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
@@ -410,6 +414,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
+
+# Inherit from the OnePlus Camera makefile.
+$(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
